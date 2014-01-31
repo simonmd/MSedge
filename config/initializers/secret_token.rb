c69d07cd8dadd800bf4d2480ae03a93dc1bf9707
Simon Rascovsky <simonmd@gmail.com>
2014-01-30 21:29:47 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MSedge::Application.config.secret_key_base = 'a7683d893e85b997375c6874aaebbc0483109f99fab86ae9fa2d23c3a4ca7e62fc0df30c258aaaca3f80b83b49ac60dfc8efe3c11b10759ea13d6cb937a638a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MSedge::Application.config.session_store :cookie_store, key: '_MSedge_session'
